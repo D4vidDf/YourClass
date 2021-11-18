2bc59d148cb1f5ce6946671af9996ae2382960f6
David Df <dominguezfondodavid@gmail.com>
2021-11-18 16:25:02 +0100

    Delete src directory

--- src/main/java/com/d4viddf/Controller/DBViewController.java
+++
@@ -1,9 +0,0 @@
-package com.d4viddf.Controller;
-
-import com.d4viddf.Factory.MySQLDAOFactory;
-
-public class DBViewController {
-
-    protected MySQLDAOFactory mySQLDAOFactory = new MySQLDAOFactory();
-    
-}
--- src/main/java/com/d4viddf/Launch.java
+++
@@ -1,7 +0,0 @@
-package com.d4viddf;
-
-public class Launch {
-    public static void main(String[] args) {
-        Main.main(args);
-    }
-}
